99368b95917a2d92891d4f60e83006a1d8da5d74
MaXqIvanov <maksivanov35@ya.ru>
2022-05-13 16:26:31 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+  <div class="main">
+    <input v-model="inputValueFrom" placeholder="цена от">
+    <input v-model="inputValueTo" class="input_price" placeholder="цена до">
+    <button @click="setSortCourses({checkedSorting, inputValueFrom, inputValueTo})"
+    class="button_price">поиск</button>
+     <div class="form-check form-switch">
+      <label class="form-check-label" for="flexSwitchCheckChecked">
+        <input class="form-check-input" type="checkbox" role="switch"
+      id="flexSwitchCheckChecked" aria-checked="true"
+      v-model="checkedSorting"
+      @change="setSortCourses({checkedSorting, inputValueFrom, inputValueTo})">
+        сортировка по цене</label>
+    </div>
+    <div v-if="sortCourses.length > 0">
+      <ul :key="elem.name" v-for="(elem) in sortCourses">
+      <li class="arrayCourses">
+        <div>
+          {{ elem.name }}
+        </div>
+        <div class="price">
+          {{ elem.prices }}
+        </div>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </div>
+    <div class="not_found" v-else>К сожалению курсов по такой цене нет</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapActions, mapMutations, mapState } from 'vuex';
+import '../scss/main.scss';
 
 export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
+  data() {
+    return {
+      inputValueFrom: '',
+      inputValueTo: '',
+      checkedSorting: false,
+    };
+  },
+  methods: {
+    ...mapMutations({
+      setSortCourses: 'setSortCourses',
+    }),
+  },
+  computed: mapState({
+    courses: (state:any) => state.courses,
+    sortCourses: (state:any) => state.sortCourses,
+  }),
 });
 </script>
 
@@ -53,10 +65,11 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
+  display: flex;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 0px;
 }
 a {
   color: #42b983;
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,66 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    courses: [
+      { name: 'Courses in England', prices: [0, 100] },
+      { name: 'Courses in Germany', prices: [500, null] },
+      { name: 'Courses in Italy', prices: [100, 200] },
+      { name: 'Courses in Russia', prices: [null, 400] },
+      { name: 'Courses in China', prices: [50, 250] },
+      { name: 'Courses in USA', prices: [200, null] },
+      { name: 'Courses in Kazakhstan', prices: [56, 324] },
+      { name: 'Courses in France', prices: [null, null] },
+    ],
+    sortCourses: [
+      { name: 'Courses in England', prices: [0, 100] },
+      { name: 'Courses in Germany', prices: [500, null] },
+      { name: 'Courses in Italy', prices: [100, 200] },
+      { name: 'Courses in Russia', prices: [null, 400] },
+      { name: 'Courses in China', prices: [50, 250] },
+      { name: 'Courses in USA', prices: [200, null] },
+      { name: 'Courses in Kazakhstan', prices: [56, 324] },
+      { name: 'Courses in France', prices: [null, null] },
+    ],
+    needSorting: false,
   },
   getters: {
   },
   mutations: {
+    setSortCourses(state, data:any) {
+      if (data.inputValueFrom === '' && data.inputValueTo === '') {
+        state.sortCourses = state.courses;
+      }
+      if (data.inputValueFrom.length > 0 && data.inputValueTo.length > 0) {
+        state.sortCourses = state.courses.filter((elem:any) => elem
+          .prices[0] >= data.inputValueFrom && elem
+          .prices[1] <= data.inputValueTo && elem.prices[0] <= data
+          .inputValueTo && (elem.prices[0] !== null || elem
+          .prices[1] !== null));
+      } else if (data.inputValueFrom.length > 0) {
+        state.sortCourses = state.courses.filter((elem:any) => elem
+          .prices[0] >= data.inputValueFrom);
+      } else if (data.inputValueTo.length > 0) {
+        state.sortCourses = state.courses.filter((elem:any) => elem
+          .prices[1] <= data.inputValueTo && elem.prices[0] <= data
+          .inputValueTo && elem
+          .prices[1] !== null);
+      }
+      if (data.checkedSorting === false) {
+        state.sortCourses.sort((a:any, b:any) => {
+          if (a.prices[0] === null) {
+            return a.prices[1] < b.prices[0] ? -1 : 1;
+          }
+          return a.prices[0] < b.prices[0] ? -1 : 1;
+        });
+      } else {
+        state.sortCourses.sort((a:any, b:any) => {
+          if (a.prices[0] === null) {
+            return 1;
+          }
+          return b.prices[0] - a.prices[0];
+        });
+      }
+    },
   },
   actions: {
   },
